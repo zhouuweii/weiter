a8318f3b01c28815e71c6e8088bc35a5a66caa40
zhouuweii <zhouuweii@163.com>
2022-05-23 20:21:29 +0800

    :bookmark: weiter-visual

--- weiter-visual/weiter-monitor/src/main/java/com/zw/weiter/modules/monitor/WeiterMonitorApplication.java
+++ weiter-visual/weiter-monitor/src/main/java/com/zw/weiter/modules/monitor/WeiterMonitorApplication.java
@@ -1,21 +1,24 @@
 package com.zw.weiter.modules.monitor;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 /**
  * 监控中心
- *
  * @author ZhouWei
  */
 @EnableAdminServer
 @SpringBootApplication
-public class WeiterMonitorApplication
-{
-    public static void main(String[] args)
-    {
-        SpringApplication.run(WeiterMonitorApplication.class, args);
-        System.out.println("监控中心启动成功   ლ(´ڡ`ლ)ﾞ  ");
-    }
+public class WeiterMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeiterMonitorApplication.class, args);
+		System.out.println("监控服务启动成功!\n" +
+				" $$$$$$$  $$    $$   $$$$$$$   $$$$$$$   $$$$$$    $$$$$$$  $$$$$$$\n" +
+				"$$        $$    $$  $$        $$        $$    $$  $$       $$       \n" +
+				" $$$$$$   $$    $$  $$        $$        $$$$$$$$   $$$$$$   $$$$$$ \n" +
+				"      $$  $$    $$  $$        $$        $$              $$       $$\n" +
+				"$$$$$$$    $$$$$$    $$$$$$$   $$$$$$$   $$$$$$$  $$$$$$$  $$$$$$$ \n");
+	}
 }
